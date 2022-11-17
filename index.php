bbf0fe5c7c7d878c267971acb72d4de9fcd0f2ef
VolLove <103765155+VolLove@users.noreply.github.com>
2022-11-17 12:23:44 +0700

    QuocViet update 12:23/17/11/2022

--- index.php
+++ index.php
@@ -197,46 +197,31 @@ require "models/productmodels.php";
             <!-- row -->
             <div class="row">
                 <!-- shop -->
+                <?php
+                $num = 0;
+                foreach ($getallType as $value) :
+                    if ($num < 3) :
+                        $num++;
+                ?>
+
                 <div class="col-md-4 col-xs-6">
                     <div class="shop">
                         <div class="shop-img">
-                            <img src="./img/shop01.png" alt="">
+                            <img src="img/shop<?php echo $value['type_id'] ?>.png" alt="">
                         </div>
                         <div class="shop-body">
-                            <h3>Laptop<br>Collection</h3>
+                            <h3><?php echo $value['type_name'] ?><br>Collection</h3>
                             <a href="#" class="cta-btn">Shop now <i class="fa fa-arrow-circle-right"></i></a>
                         </div>
                     </div>
                 </div>
+                <?php
+                    endif;
+                endforeach;
+                ?>
+
                 <!-- /shop -->
 
-                <!-- shop -->
-                <div class="col-md-4 col-xs-6">
-                    <div class="shop">
-                        <div class="shop-img">
-                            <img src="./img/shop03.png" alt="">
-                        </div>
-                        <div class="shop-body">
-                            <h3>Accessories<br>Collection</h3>
-                            <a href="#" class="cta-btn">Shop now <i class="fa fa-arrow-circle-right"></i></a>
-                        </div>
-                    </div>
-                </div>
-                <!-- /shop -->
-
-                <!-- shop -->
-                <div class="col-md-4 col-xs-6">
-                    <div class="shop">
-                        <div class="shop-img">
-                            <img src="./img/shop02.png" alt="">
-                        </div>
-                        <div class="shop-body">
-                            <h3>Cameras<br>Collection</h3>
-                            <a href="#" class="cta-btn">Shop now <i class="fa fa-arrow-circle-right"></i></a>
-                        </div>
-                    </div>
-                </div>
-                <!-- /shop -->
             </div>
             <!-- /row -->
         </div>
